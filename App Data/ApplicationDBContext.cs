d5291755b8b028cca932b2c1b84d37bc7eeedb3a
sadeel16 <30291492+sadeel16@users.noreply.github.com>
2022-08-11 11:37:03 +0300

    edit

---
+++ App Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.App_Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+
+    }
+}
