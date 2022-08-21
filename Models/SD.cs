cb7760e222f4ba4311a87e18e8136840de9bd14a
Salem-Mufarreh <salemmuf9@gmail.com>
2022-08-21 15:17:04 +0300

    SD

--- Models/SD.cs
+++ Models/SD.cs
@@ -7,5 +7,14 @@ namespace BookStore.Models
 {
     public class SD
     {
+<<<<<<< HEAD
+=======
+        public const string available = "Available";
+        public const string notAvailable = "Not Available";
+        public const string Admin ="admin";
+        public const string Locked = "Locked";
+        public const string UnLocked = "UnLocked";  
+
+>>>>>>> 47d9b6e (fixed SD.cs)
     }
 }
