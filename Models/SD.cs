1819fceb0e0fc7eb9a44f48f2ce1f104ff904161
Salem-Mufarreh <salemmuf9@gmail.com>
2022-08-21 15:18:18 +0300

    Update SD

--- Models/SD.cs
+++ Models/SD.cs
@@ -7,14 +7,14 @@ namespace BookStore.Models
 {
     public class SD
     {
-<<<<<<< HEAD
-=======
+
         public const string available = "Available";
         public const string notAvailable = "Not Available";
-        public const string Admin ="admin";
+        public const string Admin = "admin";
         public const string Locked = "Locked";
         public const string UnLocked = "UnLocked";
+        public const string Customer = "Customer";
 
->>>>>>> 47d9b6e (fixed SD.cs)
+
     }
 }
